72a53366186e2b09942cfbf50b139c23a5e0c89b
Mateo Guynn <mjguynn@users.noreply.github.com>
2021-03-21 19:18:35 -0400

    [Config] Complete config refactor

--- config.ml
+++ config.ml
@@ -24,7 +24,7 @@ let rec starts_with str sub =
 (** [ch @^ str] concatenates [str] onto [ch].*)
 let ( @^ ) ch str = String.make 1 ch ^ str
 
-let chop str n =
+let drop str n =
   try String.sub str n (String.length str - n) with _ -> ""
 
 let help () =
@@ -37,151 +37,16 @@ let help () =
      equation and save it to the specified output file, or \
      \"graph.svg\" if none was specified. Default mode. \n\
      \t\"-o\", \"--output\": Filename to save output to.\n\
-     \t\"-x <float>\", \"--xmin <float>\": Set the low end of the \
+     \t\"-x <float>\", \"--xmin=<float>\": Set the low end of the \
      domain.\n\
-     \t\"-X <float>\", \"--xmax <float>\": Set the high end of the \
+     \t\"-X <float>\", \"--xmax=<float>\": Set the high end of the \
      domain.\n\
-     \t\"-y <float>\", \"--ymin <float>\": Set the low end of the range.\n\
-     \t\"-Y <float>\", \"--ymax <float>\": Set the high end of the \
+     \t\"-y <float>\", \"--ymin=<float>\": Set the low end of the range.\n\
+     \t\"-Y <float>\", \"--ymax=<float>\": Set the high end of the \
      range.\n\
      \t\"-h\", \"--help\": Print this help dialog and don't perform \
      any actual work. \n";
   exit 0
-
-let arg_parse_long_param (arg : string) (tail : string list) =
-  match (String.split_on_char '=' arg, tail) with
-  | [ opt ], param :: new_tail -> Ok (opt, param, new_tail)
-  | opt :: f :: r, _ ->
-      Ok (opt, List.fold_left (fun a b -> a ^ '=' @^ b) f r, tail)
-  | _ -> Error ("\"" ^ arg ^ "\": malformed argument")
-
-let rec arg_parse_long (arg : string) (tail : string list) (cfg : t) =
-  match arg with
-  | "graph" -> Ok ({ cfg with command = Graph }, tail)
-  | "points" -> Ok ({ cfg with command = Points }, tail)
-  | "roots" -> Ok ({ cfg with command = Roots }, tail)
-  | "extrema" -> Ok ({ cfg with command = Extrema }, tail)
-  | "help" -> help ()
-  | x -> (
-      match arg_parse_long_param x tail with
-      | Ok (arg, str, new_tail) -> (
-          if arg = "output" then
-            Ok ({ cfg with output_file = Some str }, new_tail)
-          else
-            let x1, x2 = cfg.domain in
-            let y1, y2 = cfg.range in
-            match (arg, float_of_string_opt str) with
-            | "xmin", Some flt ->
-                Ok ({ cfg with domain = (flt, x2) }, new_tail)
-            | "xmax", Some flt ->
-                Ok ({ cfg with domain = (x1, flt) }, new_tail)
-            | "ymin", Some flt ->
-                Ok ({ cfg with range = (flt, y2) }, new_tail)
-            | "ymax", Some flt ->
-                Ok ({ cfg with range = (y1, flt) }, new_tail)
-            | "xmin", None | "xmax", None | "ymin", None | "ymax", None
-              ->
-                Error
-                  ( "Could not convert parameter for " ^ arg
-                  ^ " to string" )
-            | _ -> Error ("Option " ^ arg ^ " is unknown") )
-      | Error e -> Error e )
-
-let arg_parse_short_param (rest : string) (tail : string list) =
-  if rest <> "" then Some (rest, tail)
-  else
-    match tail with
-    | [] -> None
-    | next :: next_tail -> Some (next, next_tail)
-
-let rec arg_parse_short (arg : string) (tail : string list) (cfg : t) =
-  if arg = "" then Ok (cfg, tail)
-  else
-    let rest = chop arg 1 in
-    match arg.[0] with
-    (* Check all the zero-parameter options *)
-    | 'g' -> arg_parse_short rest tail { cfg with command = Graph }
-    | 'p' -> arg_parse_short rest tail { cfg with command = Points }
-    | 'r' -> arg_parse_short rest tail { cfg with command = Roots }
-    | 'e' -> arg_parse_short rest tail { cfg with command = Extrema }
-    | 'h' -> help ()
-    (* Check the parametric options *)
-    | c -> (
-        match arg_parse_short_param rest tail with
-        | None -> Error ("Unknown zero-parameter option " ^ c @^ "")
-        | Some (pstr, new_tail) -> (
-            let x1, x2 = cfg.domain in
-            let y1, y2 = cfg.range in
-            match (c, float_of_string_opt pstr) with
-            | 'o', _ ->
-                Ok ({ cfg with output_file = Some pstr }, new_tail)
-            | 'x', Some flt ->
-                Ok ({ cfg with domain = (flt, x2) }, new_tail)
-            | 'X', Some flt ->
-                Ok ({ cfg with domain = (x1, flt) }, new_tail)
-            | 'y', Some flt ->
-                Ok ({ cfg with range = (flt, y2) }, new_tail)
-            | 'Y', Some flt ->
-                Ok ({ cfg with range = (y1, flt) }, new_tail)
-            | 'x', None | 'X', None | 'y', None | 'Y', None ->
-                Error
-                  ( "Could not convert parameter for " ^ c
-                  @^ " to string" )
-            | _ -> Error ("Option " ^ c @^ " is unknown") ) )
-
-let rec arg_step (eq : string option) (cfg : t) = function
-  | [] -> (
-      (* make sure we have parsed an equation *)
-      match eq with
-      | None -> Error "No equation provided"
-      | Some equation -> Ok { cfg with equation } )
-  | str :: t -> (
-      let multi_eq () = Error "Multiple equations provided" in
-      let opt_parse op chopcount =
-        match op (chop str chopcount) t cfg with
-        | Ok (new_cfg, new_tail) -> arg_step eq new_cfg new_tail
-        | Error e -> Error e
-      in
-      if str = "-" then
-        (* Convention: "-" reads from stdin *)
-        if eq <> None then multi_eq ()
-        else arg_step (Some (read_line ())) cfg t
-      else if str = "--" then
-        (* Convention: "--" treats the following argument as input. *)
-        match t with
-        | [ last ] when eq = None -> arg_step (Some last) cfg []
-        | _ -> multi_eq ()
-      else if starts_with str "--" then opt_parse arg_parse_long 2
-      else if starts_with str "-" then opt_parse arg_parse_short 1
-      else
-        match eq with
-        | None -> arg_step (Some str) cfg t
-        | Some _ -> multi_eq () )
-
-let from_argv2 argv default_domain default_range =
-  match Array.to_list argv with
-  | [] ->
-      raise (Invalid_argument "argv must contain at least one entry")
-  | [ _ ] -> Error "No arguments"
-  | _ :: args -> (
-      match
-        arg_step None
-          {
-            command = Graph;
-            equation = "";
-            domain = default_domain;
-            range = default_range;
-            output_file = None;
-          }
-          args
-      with
-      | Ok cfg ->
-          if
-            fst cfg.domain <= snd cfg.domain
-            && fst cfg.range <= snd cfg.range
-          then Ok cfg
-          else Error "Invalid domain and/or range"
-      | Error e -> Error e )
 
 type parse_rule_t =
   | Flag of (string * char option)
@@ -198,7 +63,7 @@ type parse_rule_t =
     command line, all tokens afterwards are interpreted as options.
     Similarly, if the token "-" is encountered on the command line, each
     line of stdin is interpreted as an argument. This list is in
-    left-to-right order with respect to the command line.
+    right-to-left order with respect to the command line.
 
     [flags] is the list of all flags passed on the command line, where a
     flag is defined as an option that takes no parameter. [flags] is in
@@ -228,85 +93,105 @@ type parse_argv_t = {
   opts : (string * string list) list;
 }
 
-type worker_rules_t = {
-  short_flags : (char * string) list;
-  long_flags : string list;
-  short_opts : (char * string) list;
-  long_opts : string list;
-}
-
-let worker_rules (rules : parse_rule_t list) =
-  let filter_rules f = List.filter_map f rules in
-  {
-    short_flags =
-      filter_rules (function
-        | Flag (long, Some s) -> Some (s, long)
-        | _ -> None);
-    short_opts =
-      filter_rules (function
-        | Opt (long, Some s) -> Some (s, long)
-        | _ -> None);
-    long_flags =
-      filter_rules (function Flag (long, _) -> Some long | _ -> None);
-    long_opts =
-      filter_rules (function Opt (long, _) -> Some long | _ -> None);
-  }
-
 (** Read the entirety of [ch] and return a list of strings, where each
-    entry is a line of the file (without the newline character). The
-    returned list is in the reverse order from how they appear in the
-    file.*)
+    entry is a line of the file (without the newline character).*)
 let rec read_lines (ch : in_channel) : string list =
   let rec step (acc : string list) =
     try step (input_line ch :: acc) with End_of_file -> acc
   in
-  step []
+  List.rev (step [])
 
-let replace_assoc key new_val =
-  List.map (fun (k, v) -> (k, if k = key then new_val else v))
+let prepend_assoc key new_val =
+  List.map (fun (k, v) -> (k, if k = key then new_val :: v else v))
 
 let join connector a b = a ^ connector ^ b
 
-let rec parse_worker (rules : worker_rules_t) (acc : parse_argv_t) =
-  function
-  | "--" :: t -> Ok { acc with args = t @ acc.args }
+let rec parse_long_arg token rules args acc =
+  let has_rule f = List.exists f rules in
+  match String.split_on_char '=' token with
+  | [] -> raise (Invalid_argument "Impossible state")
+  | [ arg ] ->
+      if
+        has_rule (function
+          | Flag (f, _) when f = arg -> true
+          | _ -> false)
+      then parse_worker rules args { acc with flags = arg :: acc.flags }
+      else Error ("Unrecognized long flag \"" ^ arg ^ "\"")
+  | arg :: param_head :: param_tail ->
+      if
+        has_rule (function
+          | Opt (f, _) when f = arg -> true
+          | _ -> false)
+      then
+        let param = List.fold_left (join "=") param_head param_tail in
+        parse_worker rules args
+          { acc with opts = prepend_assoc arg param acc.opts }
+      else Error ("Unrecognized long option \"" ^ arg ^ "\"")
+
+and parse_short_arg token rules args acc =
+  if token = "" then parse_worker rules args acc
+  else
+    let rest = drop token 1 in
+    let cur = token.[0] in
+    match
+      List.find_opt
+        (function
+          | Flag (_, Some s) -> s = cur
+          | Opt (_, Some s) -> s = cur
+          | _ -> false)
+        rules
+    with
+    | None -> Error ("Unrecognized short flag/option \"" ^ cur @^ "\"")
+    | Some (Flag (arg, _)) ->
+        parse_short_arg rest rules args
+          { acc with flags = arg :: acc.flags }
+    | Some (Opt (arg, _)) -> (
+        match args with
+        | _ when rest <> "" ->
+            parse_worker rules args
+              { acc with opts = prepend_assoc arg rest acc.opts }
+        | h :: t ->
+            parse_worker rules t
+              { acc with opts = prepend_assoc arg h acc.opts }
+        | [] ->
+            Error ("Short option \"" ^ arg ^ "\" requires one parameter")
+        )
+
+and parse_worker
+    (rules : parse_rule_t list)
+    (args : string list)
+    (acc : parse_argv_t) =
+  match args with
+  | "--" :: t -> Ok { acc with args = List.rev_append t acc.args }
   | "-" :: t ->
-      parse_worker rules
-        { acc with args = List.rev_append (read_lines stdin) acc.args }
-        t
+      let new_acc =
+        { acc with args = List.append (read_lines stdin) acc.args }
+      in
+      parse_worker rules t new_acc
   | token :: t ->
       if starts_with token "--" then
-        match String.split_on_char '=' token with
-        (* it must be a long flag *)
-        | [ arg ] ->
-            if List.mem arg rules.long_flags then
-              parse_worker rules { acc with flags = arg :: acc.flags } t
-            else Error ("Unrecognized option \"" ^ arg ^ "\"")
-        (* it must be a long option *)
-        | arg :: param_lst ->
-            if List.mem arg rules.long_opts then
-              let param = List.fold_left (join "=") "" param_lst in
-              failwith "Unimplemented"
-            else Error ("Unrecognized option \"" ^ arg ^ "\"")
-        | [] -> failwith "Impossible state"
-      else if starts_with token "-" then Error "uh oh"
-      else parse_worker rules { acc with args = acc.args @ [ token ] } t
+        parse_long_arg (drop token 2) rules t acc
+      else if starts_with token "-" then
+        parse_short_arg (drop token 1) rules t acc
+      else parse_worker rules t { acc with args = token :: acc.args }
   | [] -> Ok acc
 
 let parse_argv (rules : parse_rule_t list) = function
   | [] -> raise (Invalid_argument "argv must have at least one entry")
-  | name :: tail ->
-      let worker_rules = worker_rules rules in
+  | name :: args ->
       let initial =
         {
           name;
           args = [];
           flags = [];
           (* invariant: [opts] contains a key for each opt rule *)
-          opts = List.map (fun key -> (key, [])) worker_rules.long_opts;
+          opts =
+            List.filter_map
+              (function Opt (key, _) -> Some (key, []) | _ -> None)
+              rules;
         }
       in
-      parse_worker worker_rules initial tail
+      parse_worker rules args initial
 
 let extract_equation = function
   | [ eq ] -> Ok eq
@@ -334,7 +219,7 @@ let extract_bounds opts (default_min, default_max) dimension var =
     if min > max then
       Error ("Minimum bound on " ^ dimension ^ " > than maximum bound.")
     else Ok (min, max)
-  with Invalid_argument _ -> Error "Bounds must be floats"
+  with Failure _ -> Error "Bounds must be floats"
 
 (** [extract_command flags] gets the command from the flag list [flags],
     where [flags] is defined in the same way as [prase_argv_t.flags].
