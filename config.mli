6e5235d588af42ae909fe608da4bec25a27d975b
Mateo Guynn <mjguynn@users.noreply.github.com>
2021-03-21 14:45:54 -0400

    [Config] Initial implementation

--- config.mli
+++ config.mli
@@ -16,8 +16,9 @@ type command_t =
   | Roots
   | Extrema
 
-(** [from_args argv] takes an argv-style list of args [argv] and uses it
-    to construct a [Config.t]. It follows the
+(** [from_argv argv default_domain default_range] takes an argv-style
+    list of args [argv] and uses it to construct a [Config.t]. It
+    follows the
     {{:https://www.gnu.org/software/libc/manual/html_node/Argument-Syntax.html}
     GNU Program Argument Syntax Conventions}. It recognizes the
     following options:
@@ -30,15 +31,15 @@ type command_t =
 
     "-e", "--extrema": Make [Extrema] the command to be executed.
 
-    "-x=[num]", "--xmin=[num]": Set minimum value on domain
+    "-x[num]", "--xmin=[num]": Set minimum value on domain
 
-    "-X=[num]", "--xmax=[num]": Set maximum value on domain
+    "-X[num]", "--xmax=[num]": Set maximum value on domain
 
-    "-y=[num]", "--ymin=[num]": Set minumum value on range
+    "-y[num]", "--ymin=[num]": Set minumum value on range
 
-    "-Y=[num]", "--ymax=[num]": Set maximum value on range
+    "-Y[num]", "--ymax=[num]": Set maximum value on range
 
-    "-o=[file]", "--output=[file]": Write output to [file].
+    "-o[file]", "--output=[file]": Write output to [file].
 
     If "-h" or "--help" is present on the command line, program help
     will be printed to stderr, and then the program will instantly
@@ -46,8 +47,19 @@ type command_t =
 
     If the number of arguments not paired to an option is exactly one,
     that argument will be the equation. Otherwise, this function shall
-    print an error message to stderr and return none. *)
-val from_args : string array -> t option
+    return an error message.
+
+    If one or both components of the domain or range are unspecified on
+    the command line, the returned representation shall inherit their
+    values from the [default_domain] and [default_range] parameters.
+
+    If no command option is provided on the command line, the returned
+    representation will have [Graph] as the command.
+
+    If the commands are invalid in another way (unknown command, etc) an
+    error message will be returned.*)
+val from_argv :
+  string array -> float * float -> float * float -> (t, string) result
 
 (** [equation cfg] returns the unaltered equation string which the user
     supplied to [ocamlgrapher].*)
--- ocamlgrapher.ml
+++ ocamlgrapher.ml
@@ -1,6 +1,7 @@
 (** [main ()] is the entry point for ocamlgrapher. *)
 let main () =
-  let (Some cfg) = Config.from_args Sys.argv in
-  print_endline (Config.to_string cfg)
+  match Config.from_argv Sys.argv (-5.0, 5.0) (-5.0, 5.0) with
+  | Error e -> Printf.eprintf "%s\n" e
+  | Ok cfg -> print_endline (Config.to_string cfg)
 
 let () = main ()
