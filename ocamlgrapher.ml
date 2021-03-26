efbe753c0a1c764bd01d904130c4eb5dc11349c0
Mateo Guynn <mjguynn@users.noreply.github.com>
2021-03-26 19:42:20 -0400

    [main] Update defaults for range and domain

--- ocamlgrapher.ml
+++ ocamlgrapher.ml
@@ -1,6 +1,6 @@
 (** [main ()] is the entry point for ocamlgrapher. *)
 let main () =
-  match Config.from_cmdline Sys.argv stdin (-5.0, 5.0) (-5.0, 5.0) with
+  match Config.from_cmdline Sys.argv stdin (-10., 10.) (-10., 10.) with
   | Error e -> Printf.eprintf "%s\n" e
   | Ok cfg -> print_endline (Config.to_string cfg)
 
