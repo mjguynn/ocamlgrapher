f24098da9a400a36171e84c405540bed54906607
Kai Horstmann <kaihorstkit@gmail.com>
2021-03-28 16:17:10 -0400

    Added spec for Parser.compute_f_of_x

--- parser.mli
+++ parser.mli
@@ -1,1 +1,5 @@
+(** [compute_f_of_x equation x] is the numerical y-value output of [x]
+    passed into the function of [equation] *)
+val compute_f_of_x : string -> float -> float
+
 val parse : string -> unit
