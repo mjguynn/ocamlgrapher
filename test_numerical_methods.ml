6a6a3772509a4a589887255bc409bcb8b307b984
dk689@cornell.edu <dk689@cornell.edu>
2021-03-27 22:37:25 -0400

    Unsuccessful
    Still having trouble with testing. Will go to office hours tomorrow. Going to bed soon. I don't think I'll have all the tests done by tomorrow's meeting. Sorry guys.

--- test_numerical_methods.ml
+++ test_numerical_methods.ml
@@ -3,9 +3,23 @@ open Numericalmethods
 
 (* Testing of numerical_methods *)
 
-let range_test name input exp_output range =
+let output_1 : t = []
+
+let range_test name (input : (float * float) list) exp_output range =
   name >:: fun _ -> assert_equal exp_output (range_limiter input range)
 
-let suite : test = []
+let suite =
+  "Numerical Methods Suite"
+  >::: [
+         range_test "Basic Example"
+           [
+             (1.0, 2.3);
+             (0.5, 0.4);
+             (0.0, -0.5);
+             (-0.5, -0.7);
+             (-1.0, -1.7);
+           ]
+           output_1 (0.0, 0.0);
+       ]
 
 let _ = run_test_tt_main suite
