85f280303d004e8e809ed5c0ee6aea59554186e2
dk689@cornell.edu <dk689@cornell.edu>
2021-03-29 19:30:57 -0400

    List print
    Finished with the list printing. Not sure how to run ocamlgrapher. I have tested my methods, though.

--- src/ocamlgrapher.ml
+++ src/ocamlgrapher.ml
@@ -2,6 +2,7 @@
 
 open Config
 open Parser
+open Numericalmethods
 
 let rec domain_maker_help current_dom_low domain_high step_size acc =
   match current_dom_low < domain_high -. step_size with
@@ -20,41 +21,46 @@ let domain_maker domain steps =
       domain_maker_help d_low d_high step_size [ d_low ]
 
 (* get outputs from equation *)
-let rec fun_output eqt domain_list acc =
+let rec fun_output eqt domain_list range acc =
   match domain_list with
-  | [] -> List.rev acc
-  | h :: t -> fun_output eqt t ((h, compute_f_of_x eqt h) :: acc)
+  | [] -> acc |> List.rev |> get_t |> fun x -> range_limiter x range
+  | h :: t -> fun_output eqt t range ((h, compute_f_of_x eqt h) :: acc)
 
 (* Go through list of equations *)
-let rec multi_fun_outputs eqt domain_list acc =
-  match eqt with
+let rec multi_fun_outputs eqts domain_list range acc =
+  match eqts with
   | [] -> List.rev acc
   | h :: t ->
-      multi_fun_outputs t domain_list
-        (fun_output h domain_list [] :: acc)
+      multi_fun_outputs t domain_list range
+        (fun_output h domain_list range [] :: acc)
 
 let tuple_print (x, y) =
-  print_endline
-    ("[" ^ Printf.sprintf "%.5f" x ^ ", " ^ Printf.sprintf "%.5f" y
-   ^ "]")
+  print_string
+    ("[" ^ Float.to_string x ^ ", " ^ Float.to_string y ^ "]" ^ "\n")
 
 let rec tuple_list_print lst =
   match lst with
-  | [] -> print_newline ()
-  | h :: t -> tuple_print h ^ tuple_list_print t
+  | [] -> print_string "\n"
+  | (x, y) :: t ->
+      tuple_print (x, y);
+      tuple_list_print t
 
 (* Pretty Print the input-output stuff *)
 let rec pp_list_of_lists lst =
   match lst with
-  | [] -> print_newline ()
-  | h :: t -> tuple_list_print h ^ pp_list_of_lists t
+  | [] -> print_string "\n"
+  | h :: t ->
+      tuple_list_print h;
+      pp_list_of_lists t
 
 (* Graphs the given function given command line inputs. Requires that
    the command line is [Ok]. *)
-let main_grapher (config : t) =
+let main_grapher (config : Config.t) =
   let domain_list = domain_maker (domain config) (steps config) in
   let eqts = equations config in
-  let input_output = multi_fun_outputs eqts domain_list [] in
+  let input_output =
+    multi_fun_outputs eqts domain_list (range config) []
+  in
   pp_list_of_lists input_output
 
 let main () =
