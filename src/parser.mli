664eba9422ce4e9912528ddcf73bb4deac22f0c9
Kai Horstmann <kaihorstkit@gmail.com>
2021-03-29 20:20:03 -0400

    Updated parser and tokenizer spec

--- src/parser.mli
+++ src/parser.mli
@@ -1,7 +1,9 @@
+open Tokenizer
+
 (** [compute_f_of_x equation x] is the numerical y-value output of [x]
     passed into the function of [equation]. Raises: [Invalid_argument]
     if [equation] is not a valid function and [Not_found] if function is
     discontinuous at [x]. *)
-val compute_f_of_x : string -> float -> float
+val compute_f_of_x : token list -> float -> float
 
 val parse : string -> unit
--- src/tokenizer.mli
+++ src/tokenizer.mli
@@ -48,4 +48,7 @@ val is_numerical_subtoken : char -> bool
 
 val is_alpha_subtoken : char -> bool
 
+(** [tokenize equation_str ] is a tokenized list of the function
+    [equation_str] passed into the function of [equation]. Raises:
+    [Invalid_argument] if [equation] is not a valid function. *)
 val tokenize : string -> token list
