73379ec14f038623fb36ebbfb6393310e36a8c24
Kai Horstmann <kaihorstkit@gmail.com>
2021-04-01 23:41:07 -0400

    Tokenizer refactor pt. 1

--- src/tokenizer.ml
+++ src/tokenizer.ml
@@ -44,6 +44,9 @@ type token =
   | Function of m_function
   | EOF
 
+let syntax_error error =
+  raise (Invalid_argument ("Syntax error: " ^ error))
+
 let unit_token_map =
   [
     ('=', Operator Equals);
@@ -59,14 +62,51 @@ let unit_token_map =
     ('e', Constant E);
   ]
 
+let alpha_token_map =
+  [
+    ("sqrt", Function Sqrt);
+    ("abs", Function Abs);
+    ("ln", Function Ln);
+    ("log", Function Log);
+    ("pi", Constant Pi);
+    ("sin", Function Sin);
+    ("cos", Function Cos);
+    ("tan", Function Tan);
+    ("sec", Function Sec);
+    ("csc", Function Csc);
+    ("cot", Function Cot);
+    ("arcsin", Function Arcsin);
+    ("arccos", Function Arccos);
+    ("arctan", Function Arctan);
+    ("arcsec", Function Arcsec);
+    ("arccsc", Function Arccsc);
+    ("arccot", Function Arccot);
+  ]
+
 let is_numerical_subtoken ch =
   let code = Char.code ch in
   code = 46 || (code < 58 && code > 47)
 
-let is_alpha_subtoken ch = List.mem ch [ 'p'; 's'; 'a'; 'l'; 'c'; 't' ]
+let is_alpha_subtoken ch =
+  let code = Char.code ch in
+  code < 123 && code > 96
 
-let syntax_error error =
-  raise (Invalid_argument ("Syntax error: " ^ error))
+let is_starting_alpha_subtoken ch =
+  List.mem ch [ 'p'; 's'; 'a'; 'l'; 'c'; 't' ]
+
+let is_unit_token ch =
+  List.mem ch [ '='; '+'; '-'; '*'; '/'; '^'; '('; ')'; 'x'; 'y'; 'e' ]
+
+let extract_alpha_token (str : string) : token =
+  match List.assoc_opt str alpha_token_map with
+  | Some tok -> tok
+  | None ->
+      syntax_error
+        "unknown character or symbol found. [alpha token assoc failure]"
+
+(* if acc is num and cur is num add cur to acc if acc is num and cur is
+   alpha add token, re-lex cur if acc is alpha and cur is alpha add cur
+   to acc if acc is alpha and cur is num add token, re-lex cur *)
 
 let tokenize equation_str =
   let tokens = ref [] in
@@ -75,11 +115,41 @@ let tokenize equation_str =
     if String.length tokens_str <> 0 then
       let hd = tokens_str.[0] in
       let tl = String.sub tokens_str 1 (String.length tokens_str - 1) in
-      if is_numerical_subtoken hd then lex tl (acc ^ Char.escaped hd)
-      else if String.length acc <> 0 then begin
-        add_token (Constant (Number (Float.of_string acc)));
-        lex (Char.escaped hd ^ tl) ""
-      end
+      if String.length acc <> 0 then
+        let starting_acc = acc.[0] in
+        if is_numerical_subtoken hd then begin
+          if is_numerical_subtoken starting_acc then
+            lex tl (acc ^ Char.escaped hd)
+          else if is_starting_alpha_subtoken starting_acc then begin
+            add_token (extract_alpha_token acc);
+            lex (Char.escaped hd ^ tl) ""
+          end
+        end
+        else if is_alpha_subtoken hd then begin
+          if is_numerical_subtoken starting_acc then begin
+            add_token (Constant (Number (Float.of_string acc)));
+            lex (Char.escaped hd ^ tl) ""
+          end
+          else if is_starting_alpha_subtoken starting_acc then
+            lex tl (acc ^ Char.escaped hd)
+        end
+        else if is_unit_token hd then begin
+          if is_numerical_subtoken starting_acc then begin
+            add_token (Constant (Number (Float.of_string acc)));
+            lex (Char.escaped hd ^ tl) ""
+          end
+          else if is_starting_alpha_subtoken starting_acc then begin
+            add_token (extract_alpha_token acc);
+            lex (Char.escaped hd ^ tl) ""
+          end
+        end
+        else if hd = ' ' then lex tl acc
+        else
+          syntax_error
+            "unknown character or symbol found. [unit subtoken assoc \
+             failure]"
+      else if is_numerical_subtoken hd || is_starting_alpha_subtoken hd
+      then lex tl (acc ^ Char.escaped hd)
       else
         match List.assoc_opt hd unit_token_map with
         | Some tok ->
@@ -87,77 +157,16 @@ let tokenize equation_str =
             lex tl ""
         | None ->
             if hd = ' ' then lex tl ""
-            else if is_alpha_subtoken hd then
-              match hd with
-              | 'p' ->
-                  if String.sub tl 0 1 = "i" then begin
-                    add_token (Constant Pi);
-                    lex (String.sub tl 1 (String.length tl - 1)) ""
-                  end
-                  else syntax_error "1"
-              | 's' -> (
-                  match String.sub tl 0 2 with
-                  | "in" ->
-                      add_token (Function Sin);
-                      lex (String.sub tl 2 (String.length tl - 2)) ""
-                  | "ec" ->
-                      add_token (Function Sec);
-                      lex (String.sub tl 2 (String.length tl - 2)) ""
-                  | "qr" ->
-                      if String.sub tl 2 1 = "t" then begin
-                        add_token (Function Sqrt);
-                        lex (String.sub tl 3 (String.length tl - 3)) ""
-                      end
-                      else syntax_error "2"
-                  | _ -> syntax_error "3")
-              | 'a' -> (
-                  match String.sub tl 0 2 with
-                  | "bs" ->
-                      add_token (Function Abs);
-                      lex (String.sub tl 2 (String.length tl - 2)) ""
-                  | "rc" ->
-                      begin
-                        match String.sub tl 2 3 with
-                        | "sin" -> add_token (Function Arcsin)
-                        | "cos" -> add_token (Function Arccos)
-                        | "tan" -> add_token (Function Arctan)
-                        | "cot" -> add_token (Function Arccot)
-                        | "sec" -> add_token (Function Arcsec)
-                        | "csc" -> add_token (Function Arccsc)
-                        | _ -> syntax_error "4"
-                      end;
-                      lex (String.sub tl 5 (String.length tl - 5)) ""
-                  | _ -> syntax_error "5")
-              | 'l' -> (
-                  match String.sub tl 0 1 with
-                  | "n" ->
-                      add_token (Function Ln);
-                      lex (String.sub tl 1 (String.length tl - 1)) ""
-                  | "o" ->
-                      if String.sub tl 1 1 = "g" then begin
-                        add_token (Function Log);
-                        lex (String.sub tl 2 (String.length tl - 2)) ""
-                      end
-                      else syntax_error "6"
-                  | _ -> syntax_error "7")
-              | 'c' ->
-                  begin
-                    match String.sub tl 0 2 with
-                    | "os" -> add_token (Function Cos)
-                    | "sc" -> add_token (Function Csc)
-                    | _ -> syntax_error "8"
-                  end;
-                  lex (String.sub tl 2 (String.length tl - 2)) ""
-              | 't' ->
-                  if String.sub tl 0 2 = "an" then begin
-                    add_token (Function Tan);
-                    lex (String.sub tl 2 (String.length tl - 2)) ""
-                  end
-                  else syntax_error "9"
-              | _ -> ()
-            else syntax_error "10"
+            else
+              syntax_error
+                "unknown character or symbol found. [unit subtoken \
+                 assoc failure]"
     else if String.length acc <> 0 then
-      add_token (Constant (Number (Float.of_string acc)))
+      let starting_acc = acc.[0] in
+      if is_numerical_subtoken starting_acc then
+        add_token (Constant (Number (Float.of_string acc)))
+      else if is_starting_alpha_subtoken starting_acc then
+        add_token (extract_alpha_token acc)
   in
   lex equation_str "";
   List.rev !tokens
