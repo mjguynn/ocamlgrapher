3a3e3fb39feb24f04e4f1c486e8ee85a2f43f9d5
Mateo Guynn <mjguynn@users.noreply.github.com>
2021-04-24 01:50:42 -0400

    [Common] create

--- src/cmdline.ml
+++ src/cmdline.ml
@@ -1,5 +1,7 @@
 (** Implementation of module [Cmdline].*)
 
+open Common
+
 (* Unfortunately, I have to duplicate some code from the interface. *)
 type parse_rule_t =
   | Flag of (string * char option)
@@ -30,35 +32,6 @@ type t = {
   opts : (string * string list) list;
   rules : parse_rule_t list;
 }
-
-(** [starts_with str sub] returns whether [str] starts with [sub].*)
-let rec starts_with str sub =
-  try String.sub str 0 (String.length sub) = sub
-  with Invalid_argument s -> false
-
-(** [drop str n] returns [str] without the first [n] characters. If
-    [str] is shorter than [n] characters, the empty string is returned.*)
-let drop str n =
-  try String.sub str n (String.length str - n) with _ -> ""
-
-(** [concat_with join a b] returns a ^ join ^ b*)
-let concat_with j a b = a ^ j ^ b
-
-(** [prepend_assoc key x] takes an association list where keys are
-    mapped to a list. Then, for all keys = [key], [x] is cons-ed onto
-    the list.*)
-let prepend_assoc key x =
-  List.map (fun (k, v) -> (k, if k = key then x :: v else v))
-
-(** [read_lines] returns a list of strings, where each entry is a line
-    in [ch] (without the newline character). The list contains an entry
-    for each line in [ch] and in *reverse* order. Requies: [ch] is
-    readable.*)
-let rec read_lines (ch : in_channel) : string list =
-  let rec step (acc : string list) =
-    try step (input_line ch :: acc) with End_of_file -> acc
-  in
-  step []
 
 let rec parse_short_arg token args acc =
   let is_short_rule c =
---
+++ src/common.ml
@@ -0,0 +1,31 @@
+(** [Common] contains helper functions which are potentially useful to
+    multiple modules. *)
+
+(** [starts_with str sub] returns whether [str] starts with [sub].*)
+let rec starts_with str sub =
+  try String.sub str 0 (String.length sub) = sub
+  with Invalid_argument s -> false
+
+(** [drop str n] returns [str] without the first [n] characters. If
+    [str] is shorter than [n] characters, the empty string is returned.*)
+let drop str n =
+  try String.sub str n (String.length str - n) with _ -> ""
+
+(** [concat_with join a b] returns a ^ join ^ b*)
+let concat_with j a b = a ^ j ^ b
+
+(** [prepend_assoc key x] takes an association list where keys are
+    mapped to a list. Then, for all keys = [key], [x] is cons-ed onto
+    the list.*)
+let prepend_assoc key x =
+  List.map (fun (k, v) -> (k, if k = key then x :: v else v))
+
+(** [read_lines] returns a list of strings, where each entry is a line
+    in [ch] (without the newline character). The list contains an entry
+    for each line in [ch] and in *reverse* order. Requies: [ch] is
+    readable.*)
+let rec read_lines (ch : in_channel) : string list =
+  let rec step (acc : string list) =
+    try step (input_line ch :: acc) with End_of_file -> acc
+  in
+  step []
