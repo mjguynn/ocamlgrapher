f93aa702bb175114fea315152985a4d79c156e01
Mateo Guynn <mjguynn@users.noreply.github.com>
2021-04-24 02:49:53 -0400

    [Xmldom] Document

--- src/xmldom.ml
+++ src/xmldom.ml
@@ -1,13 +1,38 @@
+(** A [tag] is the "type" of a DOM element. For example, the tag of <p>
+    is "p" and the tag of "<img /> is "img.*)
 type tag = string
 
+(** An [attribute] is a key-value pair of a DOM element. For example, in
+    <img src="here.png" />, there is an attribute of
+    [("src", "here.png")].*)
 type attribute = string * string
 
+(** An [element] is an abstract representation of an element in a DOM
+    tree.
+
+    A [Text s] represents the raw text [s].
+
+    A [Comment s] represents an XML comment containing [s].
+
+    A [Item t atts] represents a DOM "item" -- that is, a DOM element
+    with tag [t] and attributes [atts] which cannot contain other
+    elements. For example, [Item "img" \[("src", "here.png")\]]
+    corresponds to <img src="here.png" /> in XML.
+
+    An [Container t atts elems] represents a DOM "container" -- that is,
+    a DOM element with tag [t] and attributes [atts] which may contain
+    other elements (i.e. has a closing tag). For example,
+    [Container "div" \[\] \[Item "img" \[("src", "here.png")\]; Item
+    "img" \[("src", "there.png")\]\]] corresponds to <div> <img
+    src="here.png" /> <img src="there.png" /> </div> *)
 type element =
   | Text of string
   | Comment of string
   | Item of tag * attribute list
   | Container of tag * attribute list * element list
 
+(** [output_xml f ~tab_level:tl dom] writes the abstract DOM tree [dom]
+    to the file [f] as XML with an indentation of [tl] tabs.*)
 let rec output_xml f ?tab_level:(tl = 0) =
   let string_of_attributes =
     List.fold_left
