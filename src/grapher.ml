49d6e0a55fce9e7e630c526b6ddac0e2ff841cea
Mateo Guynn <mjguynn@users.noreply.github.com>
2021-04-24 02:37:25 -0400

    [Grapher] Improve code, document

--- src/grapher.ml
+++ src/grapher.ml
@@ -1,3 +1,5 @@
+(** Implementation of module [Grapher].*)
+
 open Xmldom
 open Common
 
@@ -9,6 +11,7 @@ type hsv = float * float * float
     (according to the CSS3 specification) corresponding to the HSV color
     [hsv]*)
 let hsl_string_of_hsv (h, sv, v) =
+  (* This formula was taken from Wikipedia and translated into OCaml *)
   let l = v *. (1. -. (sv /. 2.)) in
   let sl =
     if l = 0. || l = 1. then 0. else (v -. l) /. Float.min l (1. -. l)
@@ -19,47 +22,58 @@ let hsl_string_of_hsv (h, sv, v) =
 (** [hsv_step hsv] is a state machine which takes [hsv] and outputs a
     new color. Any color outputted will never match an earlier color in
     the output sequence, to the extent permitted by floating-point
-    precision loss.*)
+    precision loss. *)
 let hsv_step (h, s, v) =
+  let step = 0.312 in
   (* PATTERN: hue changes first, then saturation, and finally value *)
-  let new_h = h +. 0.30 in
-  if new_h > 1. then
-    let new_s = s -. 0.30 in
-    if new_s < 0. then (new_h -. 1., 1., v /. 2.)
-    else (new_h -. 1., new_s, v)
-  else (new_h, s, v)
+  let h' = h +. step in
+  if h' > 1. then
+    let h'' = h' -. 1. in
+    let s' = s -. step in
+    if s' < 0. then (h'', 1., v /. 2.) else (h'', s', v)
+  else (h', s, v)
 
-type plot = {
+(** A [relation] contains information about a relation processed by
+    OCamlgrapher. [label] is a human-readable identifier for the
+    relation, such as its formula. [points] is a list of selected points
+    satisfying the relation, in an order such that drawing a line from
+    one point to the next is a reasonable approximation of the relation.
+    [color] is the HSV color assigned to this relation on the graph. *)
+type relation = {
   label : string;
   points : (float * float) list;
   color : hsv;
 }
 
+(** AF: Let [(x1, x2) = x_bounds] and [(y1, y2) = y_bounds]. Then an
+    instance of type [t] represents a graph of the relations [relations]
+    on a window spanning [x1..x2] on the X-axis and [y1..y2] on the
+    Y-axis.
+
+    RI: [x2 > x1] and [y2 > y1]. *)
 type t = {
-  plots : plot list;
-  domain : float * float;
-  range : float * float;
+  relations : relation list;
+  x_bounds : float * float;
+  y_bounds : float * float;
 }
 
-let create domain range = { plots = []; domain; range }
+let create x_bounds y_bounds = { relations = []; x_bounds; y_bounds }
 
 let add_plot label points g =
   let color =
-    match g.plots with
+    match g.relations with
     | [] -> (0., 1., 1.) (* initial color - bright red *)
     | h :: _ -> hsv_step h.color
   in
-  { g with plots = { label; points; color } :: g.plots }
+  { g with relations = { label; points; color } :: g.relations }
 
-let span (min, max) = max -. min
-
-let text_of ?fill:(f = "black") c x y txt =
+let create_text ?fill:(f = "black") c x y txt =
   Container
     ( "text",
       [ ("fill", f); ("class", c); ("x", x); ("y", y) ],
       [ Text txt ] )
 
-let line_of ?fill:(f = "black") c x1 x2 y1 y2 =
+let create_line ?fill:(f = "black") c x1 x2 y1 y2 =
   Item
     ( "line",
       [
@@ -71,44 +85,46 @@ let line_of ?fill:(f = "black") c x1 x2 y1 y2 =
         ("y2", y2);
       ] )
 
-let circle_of ?fill:(f = "black") c x y r =
+let create_circle ?fill:(f = "black") c x y r =
   Item
     ( "circle",
       [ ("fill", f); ("class", c); ("cx", x); ("cy", y); ("r", r) ] )
 
-let eqs_label num eq =
+let relation_label num eq =
   let col = hsl_string_of_hsv eq.color in
   let h = (num * 30) + 90 in
   Container
     ( "g",
       [],
       [
-        circle_of "relation_view_disc" ~fill:col "40" (string_of_int h)
-          "10";
-        text_of "relation_view_text" ~fill:col "60px"
+        create_circle "relation_view_disc" ~fill:col "40"
+          (string_of_int h) "10";
+        create_text "relation_view_text" ~fill:col "60px"
           (string_of_int (h + 5))
           eq.label;
       ] )
 
-let eqs_view g =
-  let labels = List.mapi eqs_label (List.rev g.plots) in
+let relations_view g =
+  let labels = List.mapi relation_label (List.rev g.relations) in
   let background =
     Item ("rect", [ ("class", "relation_view_background") ])
   in
-  let border = line_of "relation_view_line" "100%" "100%" "0" "100%" in
+  let border =
+    create_line "relation_view_line" "100%" "100%" "0" "100%"
+  in
   let header =
-    text_of "relation_view_header" "15px" "40px" "Relations"
+    create_text "relation_view_header" "15px" "40px" "Relations"
   in
   let divider =
-    line_of "relation_view_line" "0%" "100%" "60px" "60px"
+    create_line "relation_view_line" "0%" "100%" "60px" "60px"
   in
   let max_label_characters =
     List.fold_left
       (fun maxlen eq -> max maxlen (String.length eq.label))
-      0 g.plots
+      0 g.relations
   in
   let width = 60 + max 160 (12 * max_label_characters) in
-  let height = 90 + max 160 (30 * List.length g.plots) in
+  let height = 90 + max 160 (30 * List.length g.relations) in
   Container
     ( "svg",
       [ ("viewBox", Printf.sprintf "0 0 %i %i" width height) ],
@@ -118,26 +134,20 @@ let plot_view g =
   Container ("svg", [ ("viewBox", "300 0 1000 1000") ], [])
 
 let to_svg filename g =
+  (* load stylesheet, create DOM element *)
   let styles = open_in "graph_styles.css" in
-  let style_obj =
-    Container
-      ( "style",
-        [],
-        [
-          Text
-            ( read_lines styles |> List.rev
-            |> List.fold_left (concat_with "\n") "" );
-        ] )
+  let styles_str =
+    read_lines styles |> List.fold_left (fun acc s -> s ^ "\n" ^ acc) ""
   in
+  let styles_elem = Container ("style", [], [ Text styles_str ]) in
   close_in styles;
+  (* begin export *)
   let f = open_out filename in
   let dom =
     Container
       ( "svg",
-        [
-          ("xmlns", "http://www.w3.org/2000/svg"); ("height", "1000px");
-        ],
-        [ style_obj; eqs_view g ] )
+        [ ("xmlns", "http://www.w3.org/2000/svg") ],
+        [ styles_elem; relations_view g ] )
   in
   output_xml f dom;
   close_out f
