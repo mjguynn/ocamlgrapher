f7ba3d30809927e603a73eab4c7137b320547035
Mateo Guynn <mjguynn@users.noreply.github.com>
2021-04-26 17:28:33 -0400

    Document svghelpers

--- src/svghelpers.mli
+++ src/svghelpers.mli
@@ -1,3 +1,9 @@
+(** [make_text css_class attributes x y message] creates an SVG element
+    which, when drawn, creates text containing [message] at position
+    [x], [y], where [x] and [y] are valid CSS scalars (eg. "40", "12px",
+    "50%"). It is assigned the stylesheet class [css_class].
+    Additionally, the element inherits all of the XML attributes in
+    [attributes]. *)
 val make_text :
   string ->
   Xmldom.attribute list ->
@@ -6,6 +12,12 @@ val make_text :
   string ->
   Xmldom.element
 
+(** [make_circle css_class attributes x y r] creates an SVG element
+    which, when drawn, creates a circle of [r] at position [x], [y],
+    where [x], [y], and [r] are valid CSS scalars (eg. "40", "12px",
+    "50%"). It is assigned the stylesheet class [css_class].
+    Additionally, the element inherits all of the XML attributes in
+    [attributes]. *)
 val make_circle :
   string ->
   Xmldom.attribute list ->
@@ -14,9 +26,23 @@ val make_circle :
   string ->
   Xmldom.element
 
+(** [make_polyline css_class points] creates an SVG element which, when
+    drawn, draws a line between each pair of sequential points in
+    [points]. The coordinates of the points are relative to the SVG view
+    containing the resulting element. It is assigned the stylesheet
+    class [css_class]. Additionally, the element inherits all of the XML
+    attributes in [attributes]. *)
 val make_polyline :
   string -> Xmldom.attribute list -> Common.points -> Xmldom.element
 
+(** [make_region_border css_class top_left bottom_right] creates an SVG
+    element which, when drawn, creates a border around the region
+    specified by its coordinates [top_left] and [bottom_right]. The
+    coordinates of the points are relative to the SVG view containing
+    the resulting element. Each individual border line is assigned the
+    stylesheet class [css_class] and, the grouping AS A WHOLE (not each
+    individual border line) inherits all of the XML attributes in
+    [attributes]. *)
 val make_region_border :
   string ->
   Xmldom.attribute list ->
